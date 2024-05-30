0a6b473ae9d7e1f43b33b326b9f626c3aa072f13
UFO753 <ufoufo753@gmail.com>
2024-05-30 14:14:42 +0200

    Fixes for service worker

--- serviceworker.js
+++ serviceworker.js
@@ -43,6 +43,7 @@ self.addEventListener("activate", (event) => {
 });
 
 self.addEventListener("fetch", (event) => {
+  // Skip cross-origin requests, like those for Google Analytics.
   if (event.request.url.startsWith(self.location.origin)) {
     event.respondWith(
       caches.match(event.request).then((cachedResponse) => {
@@ -52,6 +53,7 @@ self.addEventListener("fetch", (event) => {
 
         return caches.open(RUNTIME).then((cache) => {
           return fetch(event.request).then((response) => {
+            // Put a copy of the response in the runtime cache.
             return cache.put(event.request, response.clone()).then(() => {
               return response;
             });
