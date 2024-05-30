345bb877538a324bbdc70b7a4794e0a04c374557
UFO753 <ufoufo753@gmail.com>
2024-05-30 12:44:20 +0200

    Added PWA

---
+++ service-worker.js
@@ -0,0 +1,43 @@
+const cacheName = "speed-app-v1";
+const assetsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/scirpt.js",
+  "/style.css",
+  "/icon-192x192.png",
+  "/icon-512x512.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      console.log("Caching assets");
+      return cache.addAll(assetsToCache);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("Service Worker activated");
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== cacheName) {
+            console.log("Clearing old cache");
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
